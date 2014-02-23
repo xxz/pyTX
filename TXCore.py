20d5ad758efa820e9d4051a1bd7362f3dcabae9b
XieXuanZhong <xxz@live.it>
2014-02-23 23:58:33 +0800

    update TXCore.py

--- TXCore.py
+++ TXCore.py
@@ -7,7 +7,90 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+import time
+import datetime
+import platform
+import os
+import urllib2
+from xml.dom import minidom
+import smtplib
+from email.mime.text import MIMEText
+
+#sleep time
+SLP_TIME = 300
+
+#if field retry to get something
+RETRY_NUM = 0
+RETRY_TIMES = 3
+
+
+
 class TXCore(object):
     def __init__(self):
         print('TXCore initialize...')
+        self.logfile = 'TX'
+        self.LOOKUPLINK='http://geoip.ubuntu.com/lookup'
 
+    def show_Msg(self, msg):
+        print '[%s] %s' % (datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'), msg)
+
+    def show_ErrMsg(self, msg):
+        print '[%s] %s' % (datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'), msg)
+
+    def get_Time(self):
+        return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+    def get_IP(self):
+        global RETRY_NUM
+        global RETRY_TIMES
+
+
+        try:
+                self.show_Msg('try to get my ip')
+                f = urllib2.urlopen(self.LOOKUPLINK)
+        except:
+                if RETRY_NUM >= RETRY_TIMES :
+                    self.show_Msg('retry too many times, sleep %d second' % SLP_TIME)
+                    time.sleep(SLP_TIME)
+                    RETRY_NUM = 0
+                else:
+                    RETRY_NUM = RETRY_NUM + 1
+                    return  self.get_IP()
+
+        try:
+                xmldoc = minidom.parse(f)
+                node = xmldoc.childNodes[0]
+                ipnode = node.childNodes[0]
+        except:
+                if RETRY_NUM >= RETRY_TIMES :
+                    self.show_Msg('retry too many times, sleep %d second' % SLP_TIME)
+                    time.sleep(SLP_TIME)
+                    RETRY_NUM = 0
+                else:
+                    RETRY_NUM = RETRY_NUM + 1
+                    return self.get_IP()
+
+
+        return ipnode.firstChild.data
+
+
+    def log_IP(self, IP):
+        self.show_Msg('LogFile:'+self.logfile)
+        try:
+            self.show_Msg('Writing LogFile:'+self.logfile)
+            logfile=open(self.logfile,'w')
+        except IOError:
+            self.show_ErrMsg('Error in open logfile')
+            exit()
+
+        logfile.write(IP)
+        logfile.close()
+
+
+    def set_LogFilePath(self, filename):
+        if(platform.system() == 'Windows'):
+            logfile_path=os.getenv('TEMP')+'\\'
+        else:
+            logfile_path='/tmp/'
+
+        self.logfile=logfile_path+filename
--- sendMail.py
+++ sendMail.py
@@ -9,6 +9,8 @@ import cookielib
 from xml.dom import minidom 
 import smtplib
 from email.mime.text import MIMEText
+import platform
+import os
 
 
 mailto_list=["dblogs@163.com"]
@@ -29,8 +31,19 @@ RETRY_TIMES = 3
 
 LOOKUPLINK='http://geoip.ubuntu.com/lookup'
 
+global IPINFO
+global LOG_FILEPATH
+global LOG_FILENAME
+LOG_FILENAME='pyTX'
 
-def showMessage(msg):
+
+
+
+def showMsg(msg):
+    print '[%s] %s' % (datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'), msg)
+
+
+def showErrMsg(msg):
     print '[%s] %s' % (datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'), msg)
 
 
@@ -41,21 +54,21 @@ def getTime():
 #delivery time
 DELIVERY_TIME = getTime()
 
-def getIP(url):
+def get_IP(url):
     global RETRY_NUM
     global RETRY_TIMES
-    
+
     try:
-            showMessage('try to get my ip')
+            showMsg('Running get_IP()')
             f = urllib2.urlopen(url)
     except:
             if RETRY_NUM >= RETRY_TIMES :
-                showMessage('retry too many times, sleep %d second' % SLP_TIME)
+                showErrMsg('retry too many times, sleep %d second' % SLP_TIME)
                 time.sleep(SLP_TIME)
                 RETRY_NUM = 0
             else:
                 RETRY_NUM = RETRY_NUM + 1
-                return getIP(url)
+                return get_IP(url)
         
     try:            
             xmldoc = minidom.parse(f)
@@ -63,12 +76,12 @@ def getIP(url):
             ipnode = node.childNodes[0]
     except:
             if RETRY_NUM >= RETRY_TIMES :
-                showMessage('retry too many times, sleep %d second' % SLP_TIME)
+                showErrMsg('retry too many times, sleep %d second' % SLP_TIME)
                 time.sleep(SLP_TIME)
                 RETRY_NUM = 0
             else:
                 RETRY_NUM = RETRY_NUM + 1
-                return getIP(url)
+                return get_IP(url)
         
         
     return ipnode.firstChild.data
@@ -84,9 +97,9 @@ def send_mail(to_list, sub, content):
     '''
     try:
         #content = content+commands.getoutput("ifconfig tun0 | grep inet | awk '{print $2}'")
-        content = getIP(LOOKUPLINK)
+        content = IPINFO
     except:
-        print showMessage('There is a probleam at get ip info')
+        print showErrMsg('There is a probleam at get ip info')
 
 
     me=mail_user+"<"+mail_user+"@"+mail_postfix+">"
@@ -107,8 +120,67 @@ def send_mail(to_list, sub, content):
         print str(e)
         return False
 
-if __name__ == '__main__':
-    if send_mail(mailto_list,"subject",""): 
-        showMessage('Deliver mail sucess!')
+def log_ip(logfile_path):
+    showMsg('LogFile:'+logfile_path)
+    try:
+        showMsg('Writing LogFile:'+logfile_path)
+        logfile=open(logfile_path,'w')
+    except IOError:
+        showErrMsg('Error in open logfile')
+        exit()
+
+    logfile.write(IPINFO)
+    logfile.close()
+
+def set_IPinfo():
+    showMsg('Running set_IPinfo()')
+    global IPINFO
+    IPINFO=get_IP(LOOKUPLINK)
+    return True
+
+def set_LogFilePath():
+    if(platform.system() == 'Windows'):
+        logfile_path=os.getenv('TEMP')+'\\'
     else:
-        showMessage('Delivery mail failure!')
+        logfile_path='/tmp/'
+
+    global LOG_FILEPATH
+    LOG_FILEPATH=logfile_path+LOG_FILENAME
+    return True
+
+###True: IP must log to mail
+###False: Has no changed 
+def check_IPStatic():
+    if os.path.isfile(LOG_FILEPATH):  
+        showMsg('Running check_IPStatic()')
+        try:
+            logfile=open(LOG_FILEPATH,'r')
+        except IOError:
+            showErrMsg('Error in open logfile')
+            check_IPStatic()
+
+        ip=logfile.readline()
+        if ip==IPINFO:          
+            return False
+        else:
+            return True
+    else:
+        showMsg('Frist running checker\n\t\t\tRunning check_IPStatic()')
+        log_ip(LOG_FILEPATH)
+        if send_mail(mailto_list,"subject",""): 
+            showMsg('Deliver mail sucess!')
+            exit()
+        else:
+            showMsg('Delivery mail failure!')
+ 
+if __name__ == '__main__':
+    set_IPinfo()
+    set_LogFilePath()
+    if(check_IPStatic()):
+        if send_mail(mailto_list,"subject",""): 
+            showMsg('Deliver mail sucess!')
+            exit()
+        else:
+            showMsg('Delivery mail failure!')
+    else:
+        showMsg('IP has no changed')
